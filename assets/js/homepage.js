8cfb78acd57afde4a00a38b8b61f2757fdc12060
Nate <natesvoboda64@gmail.com>
2021-12-31 12:34:54 -0500

    make user repo request dynamic with fetch

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,11 @@
-var getUserRepos = function(){
-    fetch("https://api.github.com/users/octocat/repos");
-};
+var getUserRepos = function(user){
+    var apiURL = "https://api.github.com/users/" + user + "/repos";
+
+    fetch(apiURL).then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        });
+    });
+}
 
 getUserRepos();
