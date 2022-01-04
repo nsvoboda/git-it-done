579c101e963eacd06a437a3f409443fced053e22
Nate <natesvoboda64@gmail.com>
2022-01-04 16:18:35 -0500

    making pages responsive and redirected

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,18 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+
+var getRepoName = function(){
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if(repoName){
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else {
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function(repo){
     console.log(repo);
@@ -17,7 +30,7 @@ var getRepoIssues = function(repo){
             });
         } 
         else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 }
@@ -62,4 +75,5 @@ var displayWarning = function(repo){
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
+// getRepoIssues();
